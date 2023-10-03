6aecbfe7890388f9a7e71910445734143ead5354
Vontrailop <davidperez@utez.edu.mx>
2023-10-03 15:50:06 -0600

    Anotaciones en búsqueda binaria

--- Unidad2/Apunte/MetodosBusqueda/Binaria/Main.java
+++ Unidad2/Apunte/MetodosBusqueda/Binaria/Main.java
@@ -15,20 +15,32 @@ public class Main {
     }
 
     public static int busquedaBinaria(int[] arr, int objetivo) {
-        int izquierda = 0;
-        int derecha = arr.length - 1;
+        //Se definen los extremos del buscador, izquierda y derecha: el primero y el último.
+        int izquierda = 0; 
+        int derecha = arr.length - 1; //Tomando en cuenta siempre que el último es el tamaño del arreglo menos, dado que empezamos en 0
 
-        while (izquierda <= derecha) {
-            int medio = izquierda + (derecha - izquierda) / 2;
+        /*Mientras izquierda sea menor a derecha, debido a que vamos a ir acortando indices, ya que a medida que vayamos
+         *identificado en qué lado se encuentra el valor, iremos acortando espacios para acercarnos al valor deseado. 
+         */
+        while (izquierda <= derecha) { 
+            // En esta linea únicamente declaramos el punto medio del arreglo
+            int medio = izquierda + (derecha - izquierda) / 2;  
 
+            //si el valor del indice medio es igual al valor buscado, la función termina y regresa el índice de medio.
             if (arr[medio] == objetivo) {
                 return medio;
             }
 
+            //si el valor buscado es mayor al medio, vamos acortando el valor de izquierda, por lo que la próxima vez que
+            //el valor de medio sea declarado, se reducirá en su calculo ya que hemos reducido el valor de izquierda
+            //pero el valor no es restado sino reasignado por el medio +1
             if (arr[medio] < objetivo) {
                 izquierda = medio + 1;
             }
-
+            /*
+             * Del mismo modo que en el caso anterior, si este no se encuentra dentro del rango, lo reduce a la mitad del
+             * lado derecho del arreglo.
+             */
             else {
                 derecha = medio - 1;
             }
